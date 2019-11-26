d3b3eea99b19ecd30447c95099521acbfacd83dc
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2019-11-26 18:34:46 -0500

    Updates

---
+++ precache-manifest.1353ad20d6b10d522f6cde711421f4e8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02c6830991ffd2adad0d800723aae04",
+    "url": "/React-burger-builder-app/index.html"
+  },
+  {
+    "revision": "9de46d55c36d87ac5720",
+    "url": "/React-burger-builder-app/static/css/3.db52c36c.chunk.css"
+  },
+  {
+    "revision": "9c23ce5c87d7def0c17e",
+    "url": "/React-burger-builder-app/static/css/4.d9c13aa2.chunk.css"
+  },
+  {
+    "revision": "7200ba22fa3353219c08",
+    "url": "/React-burger-builder-app/static/css/5.74fdd196.chunk.css"
+  },
+  {
+    "revision": "e18a610bf11aabfe0625",
+    "url": "/React-burger-builder-app/static/css/main.dd1853f1.chunk.css"
+  },
+  {
+    "revision": "1b3110b7abc14c2ed2b4",
+    "url": "/React-burger-builder-app/static/js/2.5b788de6.chunk.js"
+  },
+  {
+    "revision": "9de46d55c36d87ac5720",
+    "url": "/React-burger-builder-app/static/js/3.8e9a2a94.chunk.js"
+  },
+  {
+    "revision": "9c23ce5c87d7def0c17e",
+    "url": "/React-burger-builder-app/static/js/4.a2ac4891.chunk.js"
+  },
+  {
+    "revision": "7200ba22fa3353219c08",
+    "url": "/React-burger-builder-app/static/js/5.f02dd718.chunk.js"
+  },
+  {
+    "revision": "e18a610bf11aabfe0625",
+    "url": "/React-burger-builder-app/static/js/main.651e94c3.chunk.js"
+  },
+  {
+    "revision": "0b5f78ebb4e0449b82b7",
+    "url": "/React-burger-builder-app/static/js/runtime~main.6781794e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-burger-builder-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
